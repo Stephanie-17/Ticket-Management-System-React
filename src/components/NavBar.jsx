5bbcffa2ded7dae11a3177a8fb8ea0228e594ecf
Stephanie-17 <Stephanie>
2025-10-27 20:12:17 +0100

    improved functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/useAuth";
 const NavBar = () => {
-	const { isAuthenticated } = useAuth();
+	const { isAuthenticated, logout } = useAuth();
 	return (
 		<nav className="flex justify-between items-center p-3 lg:p-4 mb-4">
 			<Link to="/">
 				<h2 className="font-bold text-3xl">FlowDesk</h2>
 			</Link>
 			{isAuthenticated ? (
-				<button className="bg-red-600 py-(--space-sm) px-(--space-md) rounded-md font-semibold text-(--color-text-secondary) cursor-pointer ">
+				<button onClick={logout} className="bg-red-600 py-(--space-sm) px-(--space-md) rounded-md font-semibold text-(--color-text-secondary) cursor-pointer ">
 					Log Out
 				</button>
 			) : (
